19c747791f26558b40be1556682b8bd3524d62a2
Whitney Lampkin <wlampkin.davis@hotmail.com>
2017-09-12 14:18:04 -0500

    Added Diagonal Difference

--- DiagonalDifference/Program.cs
+++ DiagonalDifference/Program.cs
@@ -12,11 +12,19 @@ namespace DiagonalDifference
         {
             int n = Convert.ToInt32(Console.ReadLine());
             int[][] a = new int[n][];
+
             for (int a_i = 0; a_i < n; a_i++)
             {
                 string[] a_temp = Console.ReadLine().Split(' ');
                 a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
             }
+
+            //The code is correct, but it needs to be dynamic to satisfy all test cases...
+
+            int primarySum = a[0][0] + a[1][1] + a[2][2];
+            int secondarySum = a[0][2] + a[1][1] + a[2][0];
+
+            Console.WriteLine(Math.Abs(primarySum - secondarySum));
         }
     }
 }
